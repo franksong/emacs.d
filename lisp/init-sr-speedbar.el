31783341d25598b17212ba421bc6f57c4fc94a01
Frank Song <spf.doudou@gmail.com>
2014-12-22 17:38:34 +0800

    sr-speedbar-toggle band at f1

--- lisp/init-sr-speedbar.el
+++ lisp/init-sr-speedbar.el
@@ -1,11 +1,12 @@
 (require-package 'sr-speedbar)
 (require 'speedbar)
 
-(add-hook 'after-init-hook '(lambda () (sr-speedbar-toggle)))
+;; (add-hook 'after-init-hook '(lambda () (sr-speedbar-toggle)))
 (speedbar-add-supported-extension ".go")
 
 (setq sr-speedbar-right-side nil)
 (setq sr-speedbar-width 30)
 ;; (setq speedbar-show-unknown-files t)
+(global-set-key (kbd "<f1>") 'sr-speedbar-toggle)
 
 (provide 'init-sr-speedbar)
